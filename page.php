e11252823b94437b29ddbdf95b271cb1f8351042
Samson Zhang <wwsamson@yahoo.com>
2020-06-09 23:13:45 -0400

    simple page styling

--- page.php
+++ page.php
@@ -1,13 +1,15 @@
 <?php get_header();
 ?>
-    <div class="container content-container">
-        <?php
-        while (have_posts()) : the_post(); ?>
-            <h1><?php the_title(); ?></h1>
-            <?php
-            the_content();
-        endwhile;
-        ?>
+    <div class="container">
+        <div class="single-container content-container">
+	        <?php
+	        while (have_posts()) : the_post(); ?>
+                <h1 class="morse-font-heading"><?php the_title(); ?></h1>
+		        <?php
+		        the_content();
+	        endwhile;
+	        ?>
+        </div>
     </div>
 <?php
 get_footer();
