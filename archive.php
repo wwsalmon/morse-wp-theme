32a9a0b1f735ea193f4aa60a5ddb702c9a7c14ac
Samson Zhang <wwsamson@yahoo.com>
2020-06-10 00:15:10 -0400

    simple archive page styling

--- archive.php
+++ archive.php
@@ -1,14 +1,22 @@
 <?php get_header();
 ?>
-    <div class="container">
-        <h1><?php the_archive_title(); ?></h1>
-        <?php
-        if (have_posts()):
-            while (have_posts()) : the_post();
-                get_template_part("template_parts/article");
-            endwhile;
-        endif;
-        ?>
+    <div class="container grid-container">
+        <div class="full-header morse-font-accent morse-color-primary">
+            <h1><?php echo the_archive_title(); ?></h1>
+            <div class="full-divider morse-bg-accent"></div>
+        </div>
+        <div class="stories-container">
+		    <?php
+		    if ( have_posts() ) {
+			    while ( have_posts() ) {
+				    the_post();
+				    get_template_part( "template_parts/article" );
+		        }
+		    }
+			?>
+        </div>
+        <div class="home-right">
+        </div>
     </div>
 <?php
 get_footer();
